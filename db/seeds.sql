2bcc1c6f05998a29831b49796022b7f150697c37
tdolan21 <timmydolan21@gmail.com>
2018-11-10 09:24:02 -0500

    v1

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('','classic cheese',false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('','gouda gouda',false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('', 'not a veggie burger',false);
+
